7366dddfbe9d66cd76ab2d470c69750ea2956707
Dan <daniel.smirnov17@gmail.com>
2024-08-08 19:49:53 +0100

    added Fiber framework and set up the server, added .env

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v3"
 )
 
 func main() {
 	fmt.Println("Hello there")
+
+	app := fiber.New()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+
+	log.Fatal(app.Listen("0.0.0.0:" + port))
 }
