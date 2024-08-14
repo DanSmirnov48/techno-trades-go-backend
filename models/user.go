5d130ae2ed839b3994df85f4202fe0f7be32a8b6
Dan <daniel.smirnov17@gmail.com>
2024-08-14 22:24:21 +0100

    changed User struct
    updated type User to now use UUID as an ID
    added google/uuid package to generate id
    moved BeforeCreate & AfterCreate from main to models/user
    assigning empty user values of id, role

--- models/user.go
+++ models/user.go
@@ -1,8 +1,10 @@
 package models
 
 import (
+	"fmt"
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -20,16 +22,35 @@ type Photo struct {
 }
 
 type User struct {
-	ID        uint   `gorm:"primaryKey"`
-	FirstName string `gorm:"size:100;not null"`
-	LastName  string `gorm:"size:100;not null"`
-	Email     string `gorm:"size:100;not null;unique"`
-	Role      Role   `gorm:"size:50;not null"`
-	Photo     *Photo `gorm:"embedded;embeddedPrefix:photo_"`
-	Password  string `gorm:"size:255;not null"`
-	Active    bool   `gorm:"default:true"`
-	Verified  bool   `gorm:"default:false"`
+	ID        uuid.UUID `gorm:"type:uuid;primaryKey"`
+	FirstName string    `gorm:"size:100;not null"`
+	LastName  string    `gorm:"size:100;not null"`
+	Email     string    `gorm:"size:100;unique;not null"`
+	Role      Role      `gorm:"size:50;not null"`
+	Photo     *Photo    `gorm:"embedded;embeddedPrefix:photo_"`
+	Password  string    `gorm:"size:255;not null"`
+	Active    bool      `gorm:"default:true"`
+	Verified  bool      `gorm:"default:false"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+// BeforeCreate is a GORM hook that runs before a User is created
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	u.ID = uuid.New()
+	u.CreatedAt = time.Now()
+	u.Role = UserRole
+
+	fmt.Println("Running BeforeCreate function.")
+
+	return
+}
+
+// AfterCreate is a GORM hook that runs after a User is created
+func (u *User) AfterCreate(tx *gorm.DB) (err error) {
+	// Log the details of the created user
+	fmt.Println("Running AfterCreate function.")
+
+	return
 }
