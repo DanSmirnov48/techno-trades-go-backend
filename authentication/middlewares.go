1f6f728de52800d83d5bbd4356ea4cbafb74ff0e
Dan <daniel.smirnov17@gmail.com>
2024-09-23 19:16:11 +0100

    refactored `middlewares/auth.go` & `middlewares/server.go`
    deleted 'middlewares/auth.go' & 'middlewares/server.go'
    moved those middlewares to 'authentication' package

--- authentication/middlewares.go
+++ authentication/middlewares.go
@@ -2,9 +2,11 @@ package authentication
 
 import (
 	"strings"
+	"time"
 
 	"github.com/DanSmirnov48/techno-trades-go-backend/models"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"gorm.io/gorm"
 )
 
@@ -37,4 +39,50 @@ func (mid Middleware) AuthMiddleware(c *fiber.Ctx) error {
 	}
 	c.Locals("user", user)
 	return c.Next()
+}
+
+func (mid Middleware) RateLimiter(c *fiber.Ctx) error {
+	return limiter.New(limiter.Config{
+		// Limit the maximum number of requests per period
+		Max:        5,               // 5 requests
+		Expiration: 1 * time.Minute, // Per 1 minute
+		// Customize the response when limit is reached
+		LimitReached: func(c *fiber.Ctx) error {
+			return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{
+				"status":  "error",
+				"message": "Too many requests. Please try again later.",
+			})
+		},
+	})(c)
+}
+
+func (mid Middleware) RestrictTo(roles ...models.Role) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		user, ok := c.Locals("user").(*models.User)
+		if !ok || user == nil {
+			return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+				"status":  "error",
+				"message": "User information is missing. You do not have permission to perform this action.",
+			})
+		}
+
+		// Check if the user's role is in the allowed roles
+		hasPermission := false
+		for _, role := range roles {
+			if user.Role == role {
+				hasPermission = true
+				break
+			}
+		}
+
+		if !hasPermission {
+			return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+				"status":  "error",
+				"message": "You do not have permission to perform this action.",
+			})
+		}
+
+		// User has the required role, proceed to the next handler
+		return c.Next()
+	}
 }
